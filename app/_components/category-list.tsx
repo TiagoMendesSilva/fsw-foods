2478f13718769527b8e8252abcf016b66f725881
TiagoMendesSilva <tiagomendessilva85@gmail.com>
2024-05-07 03:00:53 -0300

    feat: add category item and list components

---
+++ app/_components/category-list.tsx
@@ -0,0 +1,23 @@
+import { db } from "../_lib/prisma";
+import CategoryItem from "./category-item";
+
+const CategoryList = async () => {
+    //pegar as categorias do banco de dados
+    const categories = await db.category.findMany();
+
+    return ( 
+
+        //renderizar um item para cada categoria    
+        <>
+            <div className="grid grid-cols-2 gap-3">
+                {categories.map((category) => (
+                    <CategoryItem key={category.id} category={category} />
+                ))}
+            </div>
+        </>
+      
+       
+    );
+}
+ 
+export default CategoryList;
