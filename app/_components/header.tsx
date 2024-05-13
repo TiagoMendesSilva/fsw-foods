eaa82d4aa0c2d4a1421a818760493a7d4063cde1
TiagoMendesSilva <tiagomendessilva85@gmail.com>
2024-05-13 16:51:09 -0300

    feat: testing  login with google auth provider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,16 @@
+"use client"
+
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
 
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 const Header = () => {
+
+    const { data } = useSession();
+
     return (  
         <div className="flex justify-between pt-6 px-5">
             <div className="relative h-[30px] w-[100px]">
@@ -11,6 +18,26 @@ const Header = () => {
                     <Image src="/logo.png" alt="FSW foods" fill className="object-cover" />
                 </Link>
             </div>
+            
+            {/* Para testar o Login */}
+
+            {data?.user?.name 
+            ? ( 
+            <div className="flex gap-1 items-center">
+                <h1>{data.user.name}</h1>
+                <Button onClick={() => signOut()}>
+                Sair
+                </Button>
+            </div> 
+            )
+            : ( 
+                <Button onClick={() => signIn()}>
+                Login
+                </Button> 
+            ) 
+            }
+            
+
             <Button size="icon" variant="outline" className="border-none bg-transparent">
                 <MenuIcon></MenuIcon>
             </Button>
